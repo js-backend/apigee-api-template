94a3a655fa9ff6041cd713d97b9d6aafb8dc8024
mazhekin <mazhekin@gmail.com>
2016-07-14 09:11:36 +0200

    json web token added for login

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,4 +1,5 @@
 var Membership = require('membership');
+var jsonWebToken = require('jsonwebtoken');
 
 module.exports = {
     register: register,
@@ -23,6 +24,13 @@ function login(req, res) {
     var storage = req.a127.config('storage');
     var membership = new Membership(storage.database);
     membership.authenticate(email, password, function(err, result) {
-        res.json(result.message);
+        if (result.success) {
+            var token = jsonWebToken.sign(result.user, storage.secret, {
+                expiresIn: 24 * 60 * 60 // expires in 24 hours
+            });
+            res.json({token: token, user: result.user, message: result.message});
+        } else {
+            res.status(400).json({message: result.message});
+        }
     });
 }
